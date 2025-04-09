208b1aa9f63649d26fa32f6bbea1530a7ca7616b
Hari.Balaji4674 <hari.balaji4674@gmail.com>
2025-04-09 14:40:02 +0900

    09-04-2025

--- Interviews/Interviews-2025/terraform.tf
+++ Interviews/Interviews-2025/terraform.tf
@@ -1,3 +1,28 @@
 module "vpc" {
   source = "value"
 }
+
+resource "aws_instance" "name" {
+  depends_on = [  ]
+
+  lifecycle {
+    create_before_destroy = 
+    ignore_changes = [  ]
+    prevent_destroy = false
+    replace_triggered_by = [  ]
+    
+  }
+}
+
+data "aws_acm_certificate" "name" {
+  
+}
+
+variable "aws_instance" {
+  
+}
+
+locals {
+  
+}
+
