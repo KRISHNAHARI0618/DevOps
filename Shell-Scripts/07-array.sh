d44853ba0faca0af796398acbb448533fe32b95d
KRISHNAHARI0618 <harivardhan9397921834@gmail.com>
2025-05-12 19:30:58 +0530

    12-05-2025-19H-30M

--- Shell-Scripts/07-array.sh
+++ Shell-Scripts/07-array.sh
@@ -11,3 +11,8 @@ countries=("india" "austrlia" "Japan" "Kolkatha")
 
 echo "My Country name is ${countries[0]}"
 echo "My Opposite country is ${countries[1]}"
+
+echo "${names[$*]}"
+
+echo "${names[*]}"
+
